c1da31ab55abfd5b368bedd609f2ebb92b977382
Luis Becerra <luisoteko100@gmail.com>
2020-03-29 22:39:23 -0500

    Segunda entrega

--- index.js
+++ index.js
@@ -53,7 +53,6 @@ const leerValores = () => {
   vcarga1 = carga1.checked ? 'Positiva': 'Negativa';
   vcarga2 = carga2.checked ? 'Positiva': 'Negativa';
   vcarga3 = carga3.checked ? 'Positiva': 'Negativa';
-
   cargas = [
     {x:parseFloat(vx1), y:parseFloat(vy1), z:parseFloat(vvalor1), polaridad:vcarga1, name:"Carga 1", color:(vcarga1=='Negativa'?'blue':'red')},
     {x:parseFloat(vx2), y:parseFloat(vy2), z:parseFloat(vvalor2), polaridad:vcarga2, name:"Carga 2", color:(vcarga2=='Negativa'?'blue':'red')},
@@ -67,7 +66,7 @@ const leerValores = () => {
     }
   }
   punto = [
-    {x:parseFloat(vpx), y:parseFloat(vpy), z:5, name: 'Punto', color:'green'}
+    {x:parseFloat(vpx), y:parseFloat(vpy), z:(0.8*Math.min(vvalor1,vvalor2,vvalor3)), name: 'Punto', color:'green'}
   ];
 };
 
@@ -95,6 +94,7 @@ const calcular = () => {
   let unitarioi31 = unitarioi(vx3,vy3,vx1,vy1,vcarga3,vcarga1);
   let unitarioj31 = unitarioj(vx3,vy3,vx1,vy1,vcarga3,vcarga1);
 
+
   let fuerza12 = Math.abs((k * vvalor1 * vvalor2) / Math.pow(distancia(vx1, vy1, vx2, vy2),2));
   let fuerza23 = Math.abs((k * vvalor2 * vvalor3) / Math.pow(distancia(vx2, vy2, vx3, vy3),2));
   let fuerza13 = Math.abs((k * vvalor1 * vvalor3) / Math.pow(distancia(vx1, vy1, vx3, vy3),2));
@@ -105,11 +105,11 @@ const calcular = () => {
 
   let fuerzatotal1x = Math.abs((fuerza21*unitarioi21)+(fuerza31*unitarioi31));
   let fuerzatotal2x = Math.abs((fuerza12*unitarioi12)+(fuerza32*unitarioi32));
-  let fuerzatotal3x = Math.abs((fuerza13*unitarioi13)+(fuerza13*unitarioi13));
+  let fuerzatotal3x = Math.abs((fuerza13*unitarioi13)+(fuerza23*unitarioi23));
 
-  let fuerzatotal1y = ((fuerza21*unitarioi21)+(fuerza31*unitarioj31));
-  let fuerzatotal2y = ((fuerza12*unitarioi12)+(fuerza32*unitarioj32));
-  let fuerzatotal3y = ((fuerza13*unitarioi13)+(fuerza13*unitarioj13));
+  let fuerzatotal1y = Math.abs((fuerza21*unitarioi21)+(fuerza31*unitarioj31));
+  let fuerzatotal2y = Math.abs((fuerza12*unitarioi12)+(fuerza32*unitarioj32));
+  let fuerzatotal3y = Math.abs((fuerza13*unitarioi13)+(fuerza23*unitarioj23));
 
   let fuerzatotal1 = Math.abs(Math.sqrt(Math.pow(fuerzatotal1x,2)+Math.pow(fuerzatotal1y,2)));
   let fuerzatotal2 = Math.abs(Math.sqrt(Math.pow(fuerzatotal2x,2)+Math.pow(fuerzatotal2y,2)));
@@ -119,50 +119,63 @@ const calcular = () => {
   let campo2 = (k*vvalor2)/Math.pow(distancia(vx2,vy2,vpx,vpy),2);
   let campo3 = (k*vvalor3)/Math.pow(distancia(vx3,vy3,vpx,vpy),2);
 
+  let energia12 = calcularEnergia(vvalor1, vvalor2, distancia(vx1,vy1,vx2,vy2));
+  let energia23 = calcularEnergia(vvalor2, vvalor3, distancia(vx2,vy2,vx3,vy3));
+  let energia13 = calcularEnergia(vvalor1, vvalor3, distancia(vx1,vy1,vx3,vy3));
 
-  document.getElementById("1-2").innerHTML = fuerza12.toExponential(2) + "N";
+  document.getElementById("1-2").innerHTML = fuerza12.toExponential(2) + "N r";
   document.getElementById("1-2x").innerHTML = "x: " + (fuerza12*unitarioi12).toExponential(2) + "N";
   document.getElementById("1-2y").innerHTML = "y: " + (fuerza12*unitarioj12).toExponential(2) + "N";
 
-  document.getElementById("2-3").innerHTML = fuerza23.toExponential(2) + "N";
+  document.getElementById("2-3").innerHTML = fuerza23.toExponential(2) + "N r";
   document.getElementById("2-3x").innerHTML = "x: " + (fuerza23*unitarioi23).toExponential(2) + "N";
   document.getElementById("2-3y").innerHTML = "y: " + (fuerza23*unitarioj23).toExponential(2) + "N";
 
-  document.getElementById("1-3").innerHTML = fuerza13.toExponential(2) + "N";
+  document.getElementById("1-3").innerHTML = fuerza13.toExponential(2) + "N r";
   document.getElementById("1-3x").innerHTML = "x: " + (fuerza13*unitarioi13).toExponential(2) + "N";
   document.getElementById("1-3y").innerHTML = "y: " + (fuerza13*unitarioj13).toExponential(2) + "N";
 
-  document.getElementById("2-1").innerHTML = fuerza21.toExponential(2) + "N";
+  document.getElementById("2-1").innerHTML = fuerza21.toExponential(2) + "N r";
   document.getElementById("2-1x").innerHTML = "x: " + (fuerza21*unitarioi21).toExponential(2) + "N";
   document.getElementById("2-1y").innerHTML = "y: " + (fuerza21*unitarioj21).toExponential(2) + "N";
 
-  document.getElementById("3-2").innerHTML = fuerza32.toExponential(2) + "N";
+  document.getElementById("3-2").innerHTML = fuerza32.toExponential(2) + "N r";
   document.getElementById("3-2x").innerHTML = "x: " + (fuerza32*unitarioi32).toExponential(2) + "N";
   document.getElementById("3-2y").innerHTML = "y: " + (fuerza32*unitarioj32).toExponential(2) + "N";
 
-  document.getElementById("3-1").innerHTML = fuerza31.toExponential(2) + "N";
+  document.getElementById("3-1").innerHTML = fuerza31.toExponential(2) + "N r";
   document.getElementById("3-1x").innerHTML = "x: " + (fuerza31*unitarioi31).toExponential(2) + "N";
   document.getElementById("3-1y").innerHTML = "y: " + (fuerza31*unitarioj31).toExponential(2) + "N";
 
-  document.getElementById("total1").innerHTML = fuerzatotal1.toExponential(2) + "N";
-  document.getElementById("total1x").innerHTML = "x: " + (fuerzatotal1*unitarioi21).toExponential(2) + "N";
-  document.getElementById("total1y").innerHTML = "y: " + (fuerzatotal1*unitarioj21).toExponential(2) + "N";
+  document.getElementById("total1").innerHTML = fuerzatotal1.toExponential(2) + "N r";
+  document.getElementById("total1x").innerHTML = "x: " + (fuerzatotal1).toExponential(2) + "N";
+  document.getElementById("total1y").innerHTML = "y: " + (fuerzatotal1).toExponential(2) + "N";
 
-  document.getElementById("total2").innerHTML = fuerzatotal2.toExponential(2) + "N";
-  document.getElementById("total2x").innerHTML = "x: " + (fuerzatotal2*unitarioi32).toExponential(2) + "N";
-  document.getElementById("total2y").innerHTML = "y: " + (fuerzatotal2*unitarioj32).toExponential(2) + "N";
+  document.getElementById("total2").innerHTML = fuerzatotal2.toExponential(2) + "N r";
+  document.getElementById("total2x").innerHTML = "x: " + (fuerzatotal2).toExponential(2) + "N";
+  document.getElementById("total2y").innerHTML = "y: " + (fuerzatotal2).toExponential(2) + "N";
 
-  document.getElementById("total3").innerHTML = fuerzatotal3.toExponential(2) + "N";
-  document.getElementById("total3x").innerHTML = "x: " + (fuerzatotal3*unitarioi31).toExponential(2) + "N";
-  document.getElementById("total3y").innerHTML = "y: " + (fuerzatotal3*unitarioj31).toExponential(2) + "N";
+  document.getElementById("total3").innerHTML = fuerzatotal3.toExponential(2) + "N r";
+  document.getElementById("total3x").innerHTML = "x: " + (fuerzatotal3).toExponential(2) + "N";
+  document.getElementById("total3y").innerHTML = "y: " + (fuerzatotal3).toExponential(2) + "N";
 
   document.getElementById("campo").innerHTML =   sumatoriaCampos(campo1,campo2,campo3).toExponential(2) + "N/C";
+  
+  document.getElementById("Energia12").innerHTML = energia12.toExponential(2);
+  document.getElementById("Energia23").innerHTML = energia23.toExponential(2);
+  document.getElementById("Energia13").innerHTML = energia13.toExponential(2);
+  document.getElementById("EnergiaTotal").innerHTML = (energia12 + energia23 + energia13).toExponential(2);
+
   loadGraphic();
 };
 
 const magnitud = (x, y) => {
   return Math.sqrt((Math.pow(x, 2) + Math.pow(y, 2)));
 };
+
+const calcularEnergia = (carga1, carga2, distancia) =>{
+  return ((k*carga1*carga2)/distancia)
+}
 
 const sumatoriaCampos = (campo1, campo2, campo3) => {
   let ai = campo1 * unitarioi(vx1, vy1, vpx, vpy, '', '.');
@@ -171,8 +184,13 @@ const sumatoriaCampos = (campo1, campo2, campo3) => {
   let bj = campo2 * unitarioj(vx2, vy2, vpx, vpy, '', '.');
   let ci = campo3 * unitarioi(vx3, vy3, vpx, vpy, '', '.');
   let cj = campo3 * unitarioj(vx3, vy3, vpx, vpy, '', '.');
-  let x = ai + bi + ci;
-  let y = aj + bj + cj;
+  let x = ai + bi;
+  let y = aj + bj;
+  if(ci!=null && cj!=null){
+	  x+=ci;
+	  y+=cj;
+  }
+  
   return magnitud(x,y);
 };
 
@@ -252,5 +270,65 @@ const loadGraphic = () => {
   chart.legend.set("itemWidth", 80);
   chart.legend.set("horizontalAlign", "center");
   console.log(cargas);
+
+  let arrowsIn = document.getElementById("arrowsIn");
+  let arrowsOut = document.getElementById("arrowsOut");
+
+  let ctx = chart.ctx.canvas.getContext('2d');
+
+
+  let xmax = 0;
+  let ymax = 0;
+
+  let tamX = ctx.canvas.width;
+  let tamY = ctx.canvas.height;
+  console.log(tamX);
+  console.log(tamY);
+
+  for(let i=0; i<cargasP.length; i++){
+    xCarga = cargasP[i].x;
+    yCarga = cargasP[i].y;
+    xmax = (xCarga>xmax)?xCarga:xmax;
+    ymax = (yCarga>ymax)?yCarga:ymax;
+  }
+  for(let i=0; i<cargasN.length; i++){
+    xCarga = cargasP[i].x;
+    yCarga = cargasP[i].y;
+    xmax = (xCarga>xmax)?xCarga:xmax;
+    ymax = (yCarga>ymax)?yCarga:ymax;
+  }
+  for(let i=0; i<punto.length; i++){
+    xCarga = cargasP[i].x;
+    yCarga = cargasP[i].y;
+    xmax = (xCarga>xmax)?xCarga:xmax;
+    ymax = (yCarga>ymax)?yCarga:ymax;
+  }
+
+  console.log(xmax);
+  console.log(ymax);
+
+  for(let i=0; i<cargasP.length; i++){
+    xCarga = cargasP[i].x;
+    yCarga = cargasP[i].y;
+    zCarga = cargasP[i].z;
+    xGrafica = tamX-(xCarga * tamX / xmax)
+    yGrafica = (yCarga * tamY / ymax)
+    tamCx = 20*zCarga
+    tamCy = 20*zCarga
+
+    ctx.drawImage(arrowsOut, chart.axisX[0].convertValueToPixel(xCarga)-(tamCx/2),chart.axisY[0].convertValueToPixel(yCarga)-(tamCy/2), tamCx,tamCy)
+  }
+  for(let i=0; i<cargasN.length; i++){
+    xCarga = cargasN[i].x;
+    yCarga = cargasN[i].y;
+    zCarga = cargasN[i].z;
+    xGrafica = tamX-(xCarga * tamX / xmax)
+    yGrafica = (yCarga * tamY / ymax)
+    tamCx = 20*zCarga
+    tamCy = 20*zCarga
+
+    ctx.drawImage(arrowsIn, chart.axisX[0].convertValueToPixel(xCarga)-(tamCx/2),chart.axisY[0].convertValueToPixel(yCarga)-(tamCy/2), tamCx,tamCy)
+  }
+
 };
 
